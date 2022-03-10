64bfc4c5d30550f05604032bb36d566bdc5278b6
ZRRJDD <17610071680@162.com>
2022-03-10 22:04:24 +0800

    首次提交，修改相关配置，可直接启动代码

---
+++ notebook/jupyter_notebook_config.py
@@ -0,0 +1,18 @@
+
+# 允许所有ip访问
+c.NotebookApp.allow_origin = '*'
+c.NotebookApp.ip='*'
+# 设置验证参数token
+c.NotebookApp.token = 'zrrjdd'
+# 解决跨域问题
+c.NotebookApp.tornado_settings = {
+    'headers': {
+        'Content-Security-Policy': "frame-ancestors self *",
+    }
+}
+# jupyter工作目录
+c.NotebookApp.notebook_dir ='/Users/zrrjdd/software/jupyter'
+# 启动时不在浏览器打开
+c.NotebookApp.open_browser = False
+# 端口
+c.NotebookApp.port = 8001
